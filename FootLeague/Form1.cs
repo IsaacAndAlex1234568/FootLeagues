9971254cb12003bd234d1bb0bf206b11870276f9
ijlonsdale <ijlonsdale@icloud.com>
2020-02-29 19:14:17 +0000

    footleage

---
+++ FootLeague/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FootLeague
+{
+    public partial class fm_loading : Form
+    {
+        public fm_loading()
+        {
+            InitializeComponent();
+        }
+
+        private void fm_loading_Load(object sender, EventArgs e)
+        {
+            tm_loading.Enabled = true;
+        }
+
+        private void tm_loading_Tick(object sender, EventArgs e)
+        {
+            tm_loading.Enabled = false;
+            Form fm_profile = new fm_profile();
+            fm_profile.Visible = true;
+            this.Hide();
+
+        }
+    }
+}
---
+++ FootLeague/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FootLeague
+{
+    public partial class fm_profile : Form
+    {
+        public fm_profile()
+        {
+            InitializeComponent();
+        }
+
+        private void fm_profile_Load(object sender, EventArgs e)
+        {
+            
+          
+        }
+    }
+}
